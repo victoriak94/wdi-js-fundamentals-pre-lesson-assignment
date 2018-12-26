464069947bf750e3db82f5b7bf0f80904e7139d5
victoriak94 <vkamila6@gmail.com>
2018-12-26 14:09:33 -0700

    Use square bracket notation to access values in object

--- pacman-objects-2.js
+++ pacman-objects-2.js
@@ -4,3 +4,11 @@ console.log(ghosts);
 
 console.log(ghosts.inky); // => 'Cyan'
 console.log(ghosts.blinky); // => 'Red'
+
+console.log(ghosts['inky']); // => 'Cyan'
+console.log(ghosts['blinky']); // => 'Red'
+
+// square bracket notation is needed when the property you want to retrieve on the object is stored in a variable
+
+const ghostName = 'clyde';
+ghosts[ghostName]; // => 'Orange'

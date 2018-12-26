e5c832cdc7364504ebdacf5339a54f61783a3a9f
victoriak94 <vkamila6@gmail.com>
2018-12-26 14:21:40 -0700

    Create switch and functions file. Create named, anonymous, and arrow function

---
+++ pacman-functions.js
@@ -0,0 +1,17 @@
+
+
+// Named function
+function eatDot() {
+  console.log("MUNCH!");
+}
+
+// Anonymous function
+// Here we assign the function to a variable instead of giving it a name directly
+var eatDot = function() {
+  console.log("MUNCH!");
+}
+
+// Arrow function (also anonymous)
+var eatDot = () => {
+  console.log("MUNCH!");
+}
